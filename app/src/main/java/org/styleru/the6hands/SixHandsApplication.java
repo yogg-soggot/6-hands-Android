29d0ced34e37c6a9baea6f54c6682d744bfeb3b6
Eugene <eugene.seager@gmail.com>
2019-03-27 13:33:05 +0300

    Delete dimens.xml and deal with VK auth

---
+++ app/src/main/java/org/styleru/the6hands/SixHandsApplication.java
@@ -0,0 +1,11 @@
+package org.styleru.the6hands;
+
+import android.app.Application;
+
+public class SixHandsApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
