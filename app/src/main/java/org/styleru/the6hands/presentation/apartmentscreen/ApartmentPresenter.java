07331ffd526375c796dae7458903099b9b098de9
yogg-soggot <nikit-iv777@ya.ru>
2019-04-21 16:32:44 +0300

    expandable text now works fine

--- app/src/main/java/org/styleru/the6hands/presentation/apartmentscreen/ApartmentPresenter.java
+++ app/src/main/java/org/styleru/the6hands/presentation/apartmentscreen/ApartmentPresenter.java
@@ -1,6 +1,10 @@
 package org.styleru.the6hands.presentation.apartmentscreen;
 
+import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
 
+@InjectViewState
 public class ApartmentPresenter extends MvpPresenter<ApartmentView> {
+
 }
+
--- app/src/main/java/org/styleru/the6hands/presentation/apartmentscreen/ApartmentView.java
+++ app/src/main/java/org/styleru/the6hands/presentation/apartmentscreen/ApartmentView.java
@@ -1,6 +1,10 @@
 package org.styleru.the6hands.presentation.apartmentscreen;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 public interface ApartmentView extends MvpView {
+    @StateStrategyType(SingleStateStrategy.class)
+    void setApartmentDescription(String description);
 }
