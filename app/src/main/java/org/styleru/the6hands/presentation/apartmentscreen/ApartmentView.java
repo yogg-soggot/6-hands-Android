e73e653e85d94446c9dad34432322e0f7ba1823e
yogg-soggot <nikit-iv777@ya.ru>
2019-04-21 21:52:11 +0300

    Decided to delete animation. Added methods to a presenter

--- app/src/main/java/org/styleru/the6hands/presentation/apartmentscreen/ApartmentView.java
+++ app/src/main/java/org/styleru/the6hands/presentation/apartmentscreen/ApartmentView.java
@@ -1,10 +1,18 @@
 package org.styleru.the6hands.presentation.apartmentscreen;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
 import com.arellomobile.mvp.viewstate.strategy.SingleStateStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 public interface ApartmentView extends MvpView {
     @StateStrategyType(SingleStateStrategy.class)
     void setApartmentDescription(String description);
+
+    @StateStrategyType(AddToEndSingleStrategy.class)
+    void setFacilities(boolean fridge, boolean pets, boolean wifi, boolean parking, boolean conditioning);
+
+    @StateStrategyType(AddToEndSingleStrategy.class)
+    void setSubwayLine(int subwayLine);
+
 }
